3367f46711cf66f7c1ab21312ce799e9e9db7ac3
Manisha Heera <nishahra@gmail.com>
2021-12-09 22:56:47 -0800

    colored boxes working

--- React/BoxGenerator_Assignment/src/components/Form.js
+++ React/BoxGenerator_Assignment/src/components/Form.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Form = (props) => {
+
+    const [box, setBox] = useState([]);
+    const[color, setColor] = useState('');
+
+    const changeBoxColor = (e) => {
+        e.preventDefault();
+
+        setBox ([...box, color ]);
+    }
+    
+    
+
+
     return(
         <div>
+
+            <form onSubmit= {changeBoxColor}> 
+
+                <div>
+                    <h1> Create A Colored Box! </h1>
+                    <label> Color </label>
+                    <input type ="text" name="color" onChange={ (e) => setColor(e.target.value)}></input>
+
+                </div>
+                <button> Submit Color Box! </button>
+
+            </form>
+
+            <h1> Results </h1>
+
+            {
+                box.map((color,index) => (
+                    <div key={index} style={{
+                        backgroundColor: color,
+                        height: "100px",
+                        width: "100px",
+                        display:"inline-flex",
+                        marginTop: "10px",
+                        marginRight:"10px",
+                    }}>
+                    </div>
+                ))
+            }
 
         </div>
     )

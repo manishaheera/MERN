fdd801c7bcdebc9b20ae58bf3043972b8f11215a
Manisha Heera <nishahra@gmail.com>
2022-01-11 17:52:26 -0800

    completed

--- MongoDb/JokesApiModularized_Assignment/routes/joke.routes.js
+++ MongoDb/JokesApiModularized_Assignment/routes/joke.routes.js
@@ -7,6 +7,6 @@ module.exports = (app) => {
     app.get("/api/jokes", JokeController.findAllJokes);
     app.get("/api/jokes/:id", JokeController.findOneJoke);
     app.put("/api/jokes/:id", JokeController.updateJoke);
-    app.delete("/api/jokes:id", JokeController.deleteJoke);
+    app.delete("/api/jokes/:id", JokeController.deleteJoke);
     
 }

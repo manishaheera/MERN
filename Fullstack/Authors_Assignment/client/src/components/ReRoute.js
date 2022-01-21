a518974619f02592eee5f114656e343872647184
Manisha Heera <nishahra@gmail.com>
2022-01-21 15:51:22 -0800

    link added

--- Fullstack/Authors_Assignment/client/src/components/ReRoute.js
+++ Fullstack/Authors_Assignment/client/src/components/ReRoute.js
@@ -7,7 +7,8 @@ const ReRoute = (props) => {
     return(
         <div className="wrapper"> 
 
-        <h1>Error</h1>  
+        <h1>Error </h1>  
+        <Link to={"/"} className="addNew"> Home </Link>
 
         <p> The author you're looking for does not exist. If you would like to add an author please use the create author form:
             <Link to={"/authors/new"}>  Create Author  </Link>

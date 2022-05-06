31bc2a4a4ddb46b17011b592b6d8c8ba6ace1549
Manisha Heera <nishahra@gmail.com>
2022-05-05 22:49:38 -0700

    updates

--- Notes/Tech Assesment Prep.js
+++ Notes/Tech Assesment Prep.js
@@ -156,3 +156,39 @@ console.log(Fibonacci());
 var myArray = ['a','b','c','d']
 newArray = ['start',...myArray,'end']
 console.log(newArray)
+
+// Roman to Int
+
+var romanToInt = function(s) {
+    var roman = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    }
+    sum = 0;
+    for(let i=0; i<s.length; i++){
+        let current = roman[s.charAt(i)];
+        let next = roman[s.charAt(i+1)];
+        
+        if (next){
+            if (current >= next){
+                sum += current
+            }
+            else{
+                sum +=(next-current)
+                i++
+            } 
+        }
+        else{
+            sum += current
+            }
+        }
+
+    return sum
+};
+
+console.log(romanToInt('VLCM'));
